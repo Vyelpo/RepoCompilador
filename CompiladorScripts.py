9590026dcbe8c885202dd55b3b1c96ccbe428ac8
D0maru <20martin2025@gmail.com>
2025-07-25 16:19:57 -0500

    Compilador scripts (documentacion en el readme)

--- CompiladorScripts.py
+++ CompiladorScripts.py
@@ -1,14 +1,14 @@
-import subprocess
-import os
-import sys
-from tkinter import filedialog
+import subprocess # Para realizar comandos en la terminal
+import os # Para manipular archivos y directorios
+import sys # Para cerrar el programa cuando se requiera
+from tkinter import filedialog # Para llamar un selector grafico de archivos
 
 
 class Compilacion:
     def __init__(self):
-        self.ruta = self.pedirRuta()
+        self.ruta = self.pedirRuta() 
         self.nombreClase = self.NombreClase(self.ruta)
-        self.direct = os.path.dirname(self.ruta)
+        self.direct = os.path.dirname(self.ruta) # toma el directorio del archivo cargado para ejecutar desde ahi la compilacion
         os.chdir(os.path.dirname(self.ruta))
         compilacion = subprocess.run(
             ["javac",self.nombreClase+".java"],
@@ -17,37 +17,35 @@ class Compilacion:
             text=True
         )
 
-        if compilacion.returncode == 0:
+        if compilacion.returncode == 0: # Valida que la compilacion del archivo, es decir, en la consola sí sea un java ejecutable
             print("Se cumplio la compilacion")
-            ejecucion = subprocess.run(
+            ejecucion = subprocess.run( # Realiza otra ejecucion en el cmd, pero esta vez con los .class generados para ejecutar el archivo 
                 ["java",self.ruta],
                 cwd=self.direct,
                 capture_output=True,
                 text=True
             )
             print("Res:\n", ejecucion.stdout)
-            self.RtempFiles()
+            self.RtempFiles() # Elimina los .class
         else:
             print("Error en la compilacion:\n ", compilacion.stderr)
 
-        
-
-    def RtempFiles(self):
+    def RtempFiles(self): # Elimina los archivos .class del directorio pertinente
         for x in os.listdir():
             if x.endswith(".class"):
                 os.remove(os.path.join(os.getcwd(),x))
 
-    def NombreClase(self, ruta):
+    def NombreClase(self, ruta): #Extrae el nombre de un archivo sin su extension
         return os.path.splitext(os.path.basename(self.ruta))[0]
 
-
-    def pedirRuta(self):
+    def pedirRuta(self): #Se le pide al usuario cargar un archivo .java
         ret = filedialog.askopenfilename(title="Selecciona el .java")
-        aux = os.path.splitext(ret)[1]
+        aux = os.path.splitext(ret)[1]# Extrae la extension del archivo
         if aux.endswith(".java"):
             return ret
         else:
             print("Elegiste un archivo de extension diferente")
-            sys.exit(0)
+            sys.exit(0) #Termina la ejecucion del programa abrutamente
 
-invocacion = Compilacion()
+#Ejemplo de uso
+#Compilacion()
